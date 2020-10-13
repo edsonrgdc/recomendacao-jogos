4a9d2523801bc2ee97ca8fc0d756d3c26f4597a0
edsonrgdc18 <edsonrgdc18@gmail.com>
2020-10-13 18:06:21 -0300

    recomendacao-jogos

---
+++ recomendacao-jogos.js
@@ -0,0 +1,125 @@
+
+const interesse1 = {
+    console: 'play',
+    estilo: 'plataforma',
+    ambiente: 'estratégia',
+    intensidade: 'casual',
+}
+
+const interesse2 = {
+    console: 'xbox',
+    estilo: 'corrida',
+    ambiente: 'estratégia',
+    intensidade: 'hardcore',
+}
+
+const interesse3 = {
+    console: 'xbox',
+    estilo: 'corrida',
+    ambiente: 'medieval',
+    intensidade: 'casual',
+}
+
+const interesse4 = {
+    console: 'pc',
+    estilo: 'fps',
+    ambiente: 'estratégia',
+    intensidade: 'normal',
+}
+
+const interesse5 = {
+    console: 'pc',
+    estilo: 'corrida',
+    ambiente: 'estratégia',
+    intensidade: 'hardcore',
+}
+
+const interesse6 = {
+    console: 'xbox',
+    estilo: 'corrida',
+    ambiente: 'sci-fi',
+    intensidade: 'casual',
+}
+
+const interesse7 = {
+    console: 'play',
+    estilo: 'estrategia',
+    ambiente: 'realista',
+    intensidade: 'hardcore',
+}
+
+const interesse8 = {
+    console: 'pc',
+    estilo: 'estrategia',
+    ambiente: 'medieval',
+    intensidade: 'hardcore',
+}
+
+const interesse9 = {
+    console: 'pc',
+    estilo: 'corrida',
+    ambiente: 'sci-fi',
+    intensidade: 'casual',
+}
+
+function escolherJogo(restricoes) {
+    const console = [interesse1, interesse2, interesse3, interesse4, interesse5, interesse6, interesse7, interesse8, interesse9]
+}
+
+for (let i = 0; i < console.lenght; i++) {
+    const restricoesJogador = console[i]
+    const consoleServe = restricoesJogador.console === restricoes
+    const estiloServe = restricoesJogador.estilo === restricoes
+    const ambienteServe = restricoesJogador.ambiente === restricoes
+    const intensidadeServe = restricoesJogador.intensidade === restricoes
+
+    if (serve) {
+        return restricoesJogador
+    }
+
+    return null
+}
+
+const restricoesJogador {
+    [return = Forza Motorsport,
+        console: 'xbox',
+    estilo: 'corrida',]
+
+    [return = Age of Empires,
+        console: 'pc',
+    estilo: 'estratégia',]
+
+    [return = Final Fantasy Tatics,
+        console: 'play',
+    estilo: 'estratégia',
+    intensidade: 'hardcore']
+
+    [return = F-Zero,
+    estilo: 'corrida',
+    ambiente: 'sci-fi',]
+
+    [return = Dark Souls,
+    console: 'pc',
+    intensidade: 'hardcore']
+
+    [return = Angry Birds,
+    console: 'pc',
+    intensidade: 'casual']
+
+    [return = Crash Bandicoot,
+        console: 'play',
+    estilo: 'plataforma']
+
+
+    [return = Mario Kart,
+        estilo: 'corrida',
+    intensidade: 'casual']
+
+    [return = Counter Strike,
+        console: 'pc',
+    estilo: 'fps',]
+
+    [return = Halo,
+        console: 'xbox',
+    ambiente: 'sci-fi',]
+}
